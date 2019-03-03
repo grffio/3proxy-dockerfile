265f24935b4134ac2573e1016887ece167410d82
Alexander S <alex@grff.io>
2019-03-03 11:03:06 +0300

    Add 'maxconn' env variable

--- README.md
+++ README.md
@@ -13,8 +13,9 @@ Quick Start
 ```
 $ docker run --name 3proxy -d -p 1080:1080 -e PROXY_SECRET="user:P@SSw0rd" grffio/3proxy
 ```
-- Supported Environment variables: 
-  - `PROXY_SECRET` - login and password for proxy user, format: user1:pass1,user2:pass2 (required)
+- Supported Environment variables:
+  - `PROXY_MAXCONN` - Maximum number of simulationeous connections (default 32)
+  - `PROXY_SECRET` - Login and password for proxy user, format: user1:pass1,user2:pass2 (desirable)
 
-- Exposed Ports: 
-  - 1080/tcp for socks5
+- Exposed Ports:
+  - 1080/tcp
--- run.sh
+++ run.sh
@@ -5,6 +5,13 @@ _randPass() {
     head -c 8 /dev/urandom | xxd -p
 }
 
+# Check environment variables and set default values
+_checkEnv() {
+    if [ -z "${PROXY_MAXCONN}" ]; then
+	    export PROXY_MAXCONN="32"
+	fi
+}
+
 # Create specified users or a test user
 _credCreate() {
     rm -f /etc/3proxy/credentials.conf
@@ -20,8 +27,13 @@ _credCreate() {
             echo "users ${user}:CL:${pass}" >> /etc/3proxy/credentials.conf
         done
     fi
-#echo "setuid $(id -u nobody)" >> /etc/3proxy/credentials.conf
-echo "setgid $(id -g nobody)" >> /etc/3proxy/credentials.conf
+    #echo "setuid $(id -u nobody)" >> /etc/3proxy/credentials.conf
+    echo "setgid $(id -g nobody)" >> /etc/3proxy/credentials.conf
+}
+
+# Amending variables in config file
+_configAmend() {
+    sed -i 's/_maxconn_/'"${PROXY_MAXCONN}"'/g' /etc/3proxy/3proxy.conf
 }
 
 # Starting 3proxy
@@ -46,6 +58,8 @@ _healthCheck() {
     exit 1
 }
 
+_checkEnv
 _credCreate
+_configAmend
 _start3proxy
 _healthCheck
